fec84c25dcc4694169700f5c2c25129e172a67e1
Aryan Sharma <aryans2409@gmail.com>
2026-03-06 19:11:55 +0530

    fix deployment

--- src/modules/booking/booking.route.js
+++ src/modules/booking/booking.route.js
@@ -3,6 +3,10 @@ import { bookSeatController } from "./booking.controller.js";
 
 const router = express.Router();
 
+// POST route (real booking)
 router.post("/book/:seatId", bookSeatController);
+
+// GET route (browser demo)
 router.get("/book/:seatId", bookSeatController);
+
 export default router;
